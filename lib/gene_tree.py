c77f3203d51d926b7dd1c22ecbb3c42dc5e0de06
gwct <greggwct@gmail.com>
2022-08-15 15:48:02 -0400

    fixed rootedness check

--- lib/gene_tree.py
+++ lib/gene_tree.py
@@ -4,6 +4,7 @@ import recontree as RT, reconcore as RC, global_vars as globs, sys
 def readGeneTree(gene_tree_input):
 # Reading gene tree dictionaries.
 	gene_num, gene_tree = gene_tree_input;
+	print(gene_tree);
 
 	gene_trees_filtered = {};
 
@@ -24,7 +25,7 @@ def readGeneTree(gene_tree_input):
 	num_tips = len([n for n in ginfo if ginfo[n][2] == 'tip']);
 	num_internal = len([n for n in ginfo if ginfo[n][2] != 'tip']);
 
-	if num_tips != num_internal - 1:
+	if num_internal != (num_tips - 1):
 		gene_tree = ["# This line may not contain a tree, or if so it may be unrooted or contain a polytomy -- Filtering."];
 		return gene_num, gene_tree, True;
 	# Another check for gene tree parsing and formatting errors.
