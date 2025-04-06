10625ce3dc6fbfb77246661e01636e13c1096b7c
gwct <greggwct@gmail.com>
2025-04-06 16:43:55 -0400

    more bioconda test setup

--- bioconda_test.sh
+++ bioconda_test.sh
@@ -6,26 +6,24 @@ set -x
 # Runs test for PhyloAcc, including on a small simulated dataset that contains a fasta file, mod file,
 # bed file, id subset file, and config file.
 
-TMP=$(mktemp -d)
-trap 'rm -rf $TMP' EXIT
-export TMPDIR=$TMP
-cd $TMP
+# TMP=$(mktemp -d)
+# trap 'rm -rf $TMP' EXIT
+# export TMPDIR=$TMP
+# cd $TMP
 
 echo " ** DOWNLOADING TEST DATA."
-files=(
-  "bioconda-test-cfg.yaml"
-  "id-subset.txt"
-  "ratite.mod"
-  "simu_500_200_diffr_2-1.bed"
-  "simu_500_200_diffr_2-1.noanc.fa"
-)
 
-for file in "${files[@]}"; do
-  if ! wget -q "https://github.com/phyloacc/PhyloAcc-test-data/raw/main/bioconda-test-data/$file"; then
+if ! wget -q "https://github.com/gwct/grampa/raw/main/bioconda-test-data.zip"; then
     echo "Failed to download $file" >&2
-    exit 1
-  fi
-done
+exit 1
+fi
+unzip bioconda-test-data.zip
+
 echo " ** TEST DATA DOWNLOAD OK."
 
-# echo " ** BEGIN BINARY TEST."
+echo " ** BEGIN GRAMPA TEST."
+if ! python grampa.py --tests; then
+  echo " ** ERROR: GRAMPA tests failed." >&2
+  exit 1
+fi
+echo " ** GRAMPA TEST OK."
